f916e72ee2853f4eaf3c68e72f576d651f424111
naidu-3009 <talarinishanth3009@gmail.com>
2026-08-03 14:32:20 +0530

    changes in mapper(project)

--- main/java/com/projectlove/lovable_clone/mapper/ProjectMapper.java
+++ main/java/com/projectlove/lovable_clone/mapper/ProjectMapper.java
@@ -1,21 +1,21 @@
 package com.projectlove.lovable_clone.mapper;
 
-
 import com.projectlove.lovable_clone.dto.projects.ProjectResponse;
 import com.projectlove.lovable_clone.dto.projects.ProjectSummaryResponse;
 import com.projectlove.lovable_clone.entity.Project;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.ERROR)
 public interface ProjectMapper {
-    @Mapping(source = "owner.id" ,target = "owner.userId")
+    @Mapping(source = "owner.id", target = "owner.userId")
     ProjectResponse toProjectResponse(Project project);
-    @Mapping(source = "id" ,target = "projectId")
+
+    @Mapping(source = "id", target = "projectId")
+    ProjectSummaryResponse toProjectSummaryResponse(Project project);
+
     List<ProjectSummaryResponse> toListOfProjectSummaryResponse(List<Project> projects);
 }
